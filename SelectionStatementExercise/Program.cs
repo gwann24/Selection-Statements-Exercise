aac5685f302094c85b4df65acf93b64aecc66f5e
Greg Wanner <greg@tegogroup.com>
2024-02-19 18:15:17 -0500

    Completed Exercise 1 - if,else if, else task

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            var rNum = new Random();
+            var favNumber = rNum.Next(1, 1000);
+            Console.WriteLine("Guess my favorite number between 1 and 1000");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Your guess was too low.\nMy number was {favNumber}.");
+            } else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Your guess was too high.\nMy number was {favNumber}.");
+            } else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
